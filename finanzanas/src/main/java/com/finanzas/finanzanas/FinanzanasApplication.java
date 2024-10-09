95bb02c0d4c10f35668c6faab4aa25f0e697114a
Juan Arango <juanmaracho10@gmail.com>
2024-10-09 13:11:16 -0500

    Adding base files for java application 'finanzanas'

---
+++ finanzanas/src/main/java/com/finanzas/finanzanas/FinanzanasApplication.java
@@ -0,0 +1,13 @@
+package com.finanzas.finanzanas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzanasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzanasApplication.class, args);
+	}
+
+}
---
+++ finanzanas/src/test/java/com/finanzas/finanzanas/FinanzanasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanzas.finanzanas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzanasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
